3306a9ba3dd1e758203fdc187d13f5d21550858e
airRnot1106 <yngw.rintaro@gmail.com>
2023-01-21 23:24:39 +0900

    add eslint plugin for storybook

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,6 +5,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:tailwindcss/recommended',
+    'plugin:storybook/recommended',
     'prettier',
   ],
   plugins: ['@typescript-eslint', 'tailwindcss'],
@@ -65,9 +66,10 @@ module.exports = {
     ],
     '@typescript-eslint/consistent-type-imports': [
       'error',
-      { prefer: 'type-imports' },
+      {
+        prefer: 'type-imports',
+      },
     ],
-
     /* tailwindcss */
     'tailwindcss/no-custom-classname': [
       'warn',
