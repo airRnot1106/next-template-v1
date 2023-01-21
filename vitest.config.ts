dba7addd890bb5682aa9f7be1aeb6e63b8bf6306
airRnot1106 <yngw.rintaro@gmail.com>
2023-01-22 01:29:54 +0900

    change dir name from tests to __tests__

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './src/tests/setup.ts',
+    setupFiles: './src/__tests__/setup.ts',
   },
   resolve: {
     alias: {
